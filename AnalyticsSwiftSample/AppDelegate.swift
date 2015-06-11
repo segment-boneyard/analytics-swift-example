66cbffb387419b72bcdb8b05591264ddfc382c5e
Prateek Srivastava <f2prateek@gmail.com>
2015-06-10 17:41:30 -0700

    Initial Commit

---
+++ AnalyticsSwiftSample/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  AnalyticsSwiftSample
+//
+//  Created by Prateek Srivastava on 2015-06-10.
+//  Copyright (c) 2015 Segment. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+}
